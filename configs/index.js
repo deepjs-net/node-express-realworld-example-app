3212e415f8032cf33776b09d4eae3a2a1bdc70ab
mengyue <mengyue@hujiang.com>
2019-07-26 16:13:19 +0800

    add mongoose connect

--- configs/index.js
+++ configs/index.js
@@ -4,7 +4,7 @@ module.exports = {
     mongodb: {
       host: '127.0.0.1', //
       port: 27017, //
-      db: 'n',
+      db: 'n_log',
       user: 'xiaohan',
       password: 'xiaohan123'
     },
